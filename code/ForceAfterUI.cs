06ebf2e121d4b06c229a0ef0bc3737e3b3a0ed7c
alxl <alex.cavasso@gmail.com>
2024-12-22 02:34:31 -0600

    AfterUI hack

---
+++ code/ForceAfterUI.cs
@@ -0,0 +1,18 @@
+namespace Neverspace;
+
+public class MyGameSystem : GameObjectSystem
+{
+	public MyGameSystem( Scene scene ) : base( scene )
+	{
+		Listen( Stage.StartUpdate, -1, ForceAfterUI, "ForceAfterUI" );
+	}
+
+	void ForceAfterUI()
+	{
+		foreach ( var m in Scene.GetAllComponents<ModelRenderer>() )
+		{
+            // shitty hack to circumvent the s&box bug that makes unlit materials' colors wrong
+			m.RenderOptions.AfterUI = true;
+		}
+	}
+}
