7821e1c26f1669c13467fd0d2c1abb2ed38592fc
alxl <alex.cavasso@gmail.com>
2025-01-05 01:59:07 -0600

    rearrange shrinking room

--- code/Quantum/QuantumCycle.cs
+++ code/Quantum/QuantumCycle.cs
@@ -6,14 +6,14 @@ namespace Neverspace;
 
 public sealed class QuantumCycle : QuantumController
 {
-	[Property] int ActiveStage { get; set; }
+	[Property] int ActiveStage { get; set; } = 0;
 	[Property] bool LoopCycle { get; set; } = true;
 	[Property] bool AddNullStage { get; set; } = false;
 
 	protected override void OnStart()
 	{
+		ActiveStage--; // OnUnobserve gets called in OnStart
 		base.OnStart();
-		ApplyCurrentStage();
 	}
 
 	protected override void OnObserve() { }
